d60341f6de3fd34a9e3021d1a81c278905ee99a4
cassiejones9 <55560788+cassiejones9@users.noreply.github.com>
2020-09-21 12:39:10 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,6 @@
 
     
     function displayStory() {
-            var storyHTML = document.getElementById(“story_editor”).value
-            document.getElementById(“story_display”).innerHTML = storyHTML
+            var storyHTML = document.getElementById("story_editor").value
+            document.getElementById("story_display").innerHTML = storyHTML
         }
