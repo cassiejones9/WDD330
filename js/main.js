fcb3949e387358c6b51bd2d1fdb7a4d4fd942482
cassiejones9 <55560788+cassiejones9@users.noreply.github.com>
2020-10-02 14:00:58 -0700

    Updated

--- js/main.js
+++ js/main.js
@@ -21,21 +21,43 @@
 // ol.appendChild(li);
 // });
 
-    
-    function loadStory() {
-            var storyName = document.getElementById("name_input").value;
-            var storyHTML = localStorage.getItem(storyName);
-            document.getElementById("story_editor").value = storyHTML;
-        }
-    
-    function saveStory() {
-            var storyName = document.getElementById("name_input").value;
-            var storyHTML = document.getElementById("story_editor").value;
-            localStorage.setItem(storyName, storyHTML);
-        }
-        
-    function displayStory() {
-            var storyName = document.getElementById("name_input").value;
-            var storyHTML = localStorage.getItem(storyName);
-            document.getElementById("story_display").innerHTML = storyHTML;
-        }
+
+function loadStory() {
+    var storyName = document.getElementById("name_input").value;
+    var storyHTML = localStorage.getItem(storyName);
+    document.getElementById("story_editor").value = storyHTML;
+}
+
+function saveStory() {
+    var storyName = document.getElementById("name_input").value;
+    var storyHTML = document.getElementById("story_editor").value;
+    localStorage.setItem(storyName, storyHTML);
+}
+
+function displayStory() {
+    var storyName = document.getElementById("name_input").value;
+    var storyHTML = localStorage.getItem(storyName);
+    document.getElementById("story_display").innerHTML = storyHTML;
+}
+
+const clickParagraph = document.getElementById('click');
+clickParagraph.addEventListener('click', () => console.log('click'));
+clickParagraph.addEventListener('mousedown', () => console.log('down'));
+clickParagraph.addEventListener('mouseup', () => console.log('up'));
+
+const dblclickParagraph = document.getElementById('dblclick');
+dblclickParagraph.addEventListener('dblclick', highlight);
+
+function highlight(event) {
+    event.target.classList.toggle('highlight');
+}
+
+const mouseParagraph = document.getElementById('mouse');
+mouseParagraph.addEventListener('mouseover', highlight);
+mouseParagraph.addEventListener('mouseout', highlight);
+
+const brokenLink = document.getElementById('broken');
+brokenLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('Broken Link!');
+});
