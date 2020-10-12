e95052d40ad1bb762d9992017a65a77915fd9c09
cassiejones9 <55560788+cassiejones9@users.noreply.github.com>
2020-10-12 15:29:33 -0700

    updated 10/12

--- js/taweek4.js
+++ js/taweek4.js
@@ -1,38 +1,67 @@
-const playarea = document.querySelector('.play-area');
-const item = document.querySelector('item');
-const reset = document.getElementById('reset');
-const player1 = 'X';
-const player2 = 'O';
+      const board = document.querySelector('.board');
+      const resetButton = document.getElementById('reset');
+      const player1 = 'X';
+      const player2 = 'O';
+      let player = player1;
 
-let player = player1;
+      function addPiece(e) {
+          console.log(e.target);
+          e.target.innerHTML = player;
+          if (player === player1) player = player2;
+          else player = player1;
+      }
+      // table version
+      function resetBoard() {
+          console.dir(board);
+          for (let i = 0; i < board.rows.length; i++) {
+              let row = board.rows[i];
+              for (let j = 0; j < row.cells.length; j++) {
+                  row.cells[j].innerHTML = '';
+              }
+          }
+      }
+      board.addEventListener('click', addPiece);
+      divBoard.addEventListener('click', addPiece);
+      reset.addEventListener('click', resetBoardDiv);
 
-function playGame(e) {
 
-    console.log(e.target);
-    e.target.innerHTML = player;
-    if (player === player1) {
-        player = player2;
-    } else player = player1;
-}
+      // This was the code I tried working with. It's close but it doesn't work.
 
-function resetBoard(){
-    // const playarea = document.querySelector('.play-area');
-    for (let i = 0; i < playarea.children.length; i++) {
-        playarea.chilren[i].innerText = '';
-    }
-    const children = Array.from(playarea.children);
-    const empty = children.filter(function(child){
-        return child.innerText == 'X' || child.innerText == 'O';
-    });
-    console.log(empty);
-}
+      // const playarea = document.querySelector('.play-area');
+      // const item = document.querySelector('item');
+      // const reset = document.getElementById('reset');
+      // const player1 = 'X';
+      // const player2 = 'O';
 
-const itemNum = document.getElementById('item1');
-itemNum.addEventListener('click', )
+      // let player = player1;
 
-item.addEventListener('click', (playGame) => {
-    if (child.innerText = 'X');{
-        console.log('X');
-    } else child.innerText == 'O';
-});
-reset.addEventListener('click', resetBoard);
+      // function playGame(e) {
+
+      //     console.log(e.target);
+      //     e.target.innerHTML = player;
+      //     if (player === player1) {
+      //         player = player2;
+      //     } else player = player1;
+      // }
+
+      // function resetBoard(){
+      //     // const playarea = document.querySelector('.play-area');
+      //     for (let i = 0; i < playarea.children.length; i++) {
+      //         playarea.chilren[i].innerText = '';
+      //     }
+      //     const children = Array.from(playarea.children);
+      //     const empty = children.filter(function(child){
+      //         return child.innerText == 'X' || child.innerText == 'O';
+      //     });
+      //     console.log(empty);
+      // }
+
+      // const itemNum = document.getElementById('item1');
+      // itemNum.addEventListener('click', )
+
+      // item.addEventListener('click', (playGame) => {
+      //     if (child.innerText = 'X');{
+      //         console.log('X');
+      //     } else child.innerText == 'O';
+      // });
+      // reset.addEventListener('click', resetBoard);
