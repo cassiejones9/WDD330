c32925ef93251e22d133aafbba0bb2e9428c1a4b
cassiejones9 <55560788+cassiejones9@users.noreply.github.com>
2020-11-18 13:49:06 -0800

    149

--- week10/index.js
+++ week10/index.js
@@ -1,9 +1,18 @@
-import { getJSON, getLocation } from './utilities.js';
+// import { getJSON, getLocation } from './utilities.js';
+import QuakesController from './quakesController.js';
 
-const baseUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
+// const baseUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 
-const quakes = getJSON(baseUrl);
-const location = getLocation();
-location.then(result => console.log(result.coords.latitude)); //this should be step 4 to marry location and pull quakes.
-// console.log(quake);
-console.log(location);
+
+
+// const quakes = getJSON(baseUrl);
+// const location = getLocation();
+// location.then(result => console.log(result.coords.latitude)); //this should be step 4 to marry location and pull quakes.
+
+// const baseUrl = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-03-02&latitude=${result.coords.latitude}&longitude=${result.coords.longitude}&maxradiuskm=100`;
+
+// console.log(getJSON(baseUrl));
+// console.log(location);
+
+const newQuakeController = new QuakesController ('#quakeList');
+newQuakeController.init();
